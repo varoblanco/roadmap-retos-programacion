0ebe1b3678f1adf894febc775c0a680e5f5c187b
varoblanco <varoblanco@gmail.com>
2025-09-09 18:21:33 +0200

    16 REGEX varoblanco v2

--- Roadmap/16 - EXPRESIONES REGULARES/python/varoblanco.py
+++ Roadmap/16 - EXPRESIONES REGULARES/python/varoblanco.py
@@ -3,14 +3,14 @@
 
 import re
 
-text = "Mi numero es 656 835 645 y mi correo es algo123@gmail.com siendo mi web https://espacioblanco.es"
+text = "Mi numero es 656 835 645 y mi correo es algo123@gmail.com siendo mi web https://www.espacioblanco.es"
 
 try :
     print(f"Primera palabra: {re.search(r"^\w+", text).group()}")
 
-    print(f"Buscar movil: {re.search(r"\W6\d{2}\W?\d\W?\d\W?\d\W?\d\W?\d\W?\d\s", text).group()}")
+    print(f"Buscar movil: {re.search(r"\+?[\d\s-]{3,}", text).group()}")
 
-    print(f"Buscar email: {re.search(r"\W\w*[@]\w*[.]\w*", text).group()}")
+    print(f"Buscar email: {re.findall(r"[\w+-]+@\w+[.][a-zA-Z]{2,4}", text)}")
 
     print(f"Nª 3 cifras con search: {re.search(r"\d{3}", text).group()}")
 
@@ -18,8 +18,8 @@ try :
 
     print(f"Dejar solo los ultimos 3 numeros privacidad: {re.sub(r"\d", "X", text)}")
 
-    print(f"Buscar url: {re.search(r'https?://?\w*[.]\w{2,3}', text).group()}")
+    print(f"Buscar url: {re.search(r'http[s]?://(www.)?\w+[.]\w{2,3}', text).group()}")
 
 except AttributeError:
     print(f"No se encontró regex solicitado en el texto")
-
+ 
